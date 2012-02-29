209b58fde4595c9105ba5a88ae792eb8ac9bdbd3
Oren Mazor <oren@wildbit.com>
2012-02-29 14:06:55 -0500

    updated hello world example with caching

--- widgets/hello_world.py
+++ widgets/hello_world.py
@@ -2,9 +2,24 @@
 #
 #geckoboard_types is a dictionary of available widget types as defined by geckoboard. see README.
 
-def hello_world(geckoboard_types):
+def hello_world(geckoboard_types,cache):
+
+	#get the response object to extend
 	my_widget = geckoboard_types["number_and_secondary_stat"]
-	my_widget["item"][0]["value"] = 5
-	my_widget["item"][1]["value"] = 1
+
+	#do whatever calculation you need
+	my_new_value = 5
+
+	#update the new value
+	my_widget["item"][0]["value"] = my_new_value
+
+	#get the old one if it exists
+	if "hello_world_foobar" in cache.keys():
+		my_widget["item"][1]["value"] = cache["hello_world_foobar"]
+	else:
+		my_widget["item"][1]["value"] = 0
+
+	#save for next time
+	cache["hello_world_foobar"] = my_new_value
 
 	return my_widget
