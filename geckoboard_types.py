6601a0ed5dbbcf2a9e8f47fb77d158a5feb97417
Oren Mazor <oren@wildbit.com>
2012-03-01 10:49:20 -0500

    added line chart type

--- geckoboard_types.py
+++ geckoboard_types.py
@@ -1,3 +1,11 @@
 types = {"number_and_secondary_stat":
-			{ "item" : [ { "text" : "","value" : 0},{ "text" : "","value" : 238}] }
+			{ "item" : [ { "text" : "","value" : 0},{ "text" : "","value" : 238}] },
+		"line_chart":{
+			    "item": [],
+			    "settings": {
+			     "axisx": [],
+			     "axisy": [],
+			     "colour": "ff9900"
+			    }
+			}
 		}
